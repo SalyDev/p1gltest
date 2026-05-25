2cbee2b0bc8e1f21ced61868351180df0262bb1f
Ndeye Saly Dione <ndeyesalydione@gmail.com>
2026-05-25 20:37:24 +0000

    changes 2

--- src/App.tsx
+++ src/App.tsx
@@ -301,7 +301,7 @@ export default function App() {
 
           <div className="brand">
             <div className="brand-icon">⚡</div>
-            <span className="brand-name">P1GL</span>
+            <span className="brand-name">P1GL APP</span>
           </div>
 
           {success && (
